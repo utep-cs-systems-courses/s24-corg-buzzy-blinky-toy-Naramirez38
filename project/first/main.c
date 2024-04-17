3d9a2ea6934e13ff22115bf2a07c6c46aa2faace
Naramirez38 <naramirez38@miners.utep.edu>
2024-04-16 21:05:06 -0600

    first method making light blink finished in  directory

---
+++ project/first/main.c
@@ -0,0 +1,14 @@
+//Initial test for just the LEDs make Red LED on and Green blink
+#include <msp430.h>
+#include "libTimer.h"
+#include "led.h"
+
+int main(void){
+
+  P1DIR |= LEDS;
+  P1OUT ^= ~LED_GREEN;
+  P1OUT |= ~LED_RED;
+
+  or_sr(0x18);
+
+}
