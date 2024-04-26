977baa40c3ac6f2c07b81ecc733e311b4da8b887
Naramirez38 <naramirez38@miners.utep.edu>
2024-04-25 19:05:57 -0600

    fully implemented modularization of methods and full implemetation of second based statemachine

--- project/2_test/main.c
+++ project/2_test/main.c
@@ -15,86 +15,72 @@ int main(void){
   or_sr(0x18);
 
 }
-int countsec = 0;
-int blinkLimit = 0;
-int blinkCount_r = 5;
-int blinkCount_g = 6;
 
-/*void blinkUpdate_green(){
+static int blinkLimit = 3;
+void blink_Update_Green(){
+  static int blinkCount_g = 6;
   blinkCount_g--;
   if(blinkCount_g <= blinkLimit){
     blinkCount_g=6;
-    red_control(1);
+    green_Control(1);
   } else {
-    red_control(0);
-  }
-}
-void blinkUpdate_red(){
-  blinkCount_r++;
-  if(blinkCount_r >= blinkLimit){
-    blinkCount_r = 0;
-    green_control(1);
-  } else {
-    green_control(0);
+    green_Control(0);
   }
 }
 
-void bothBlink(){
-  blinkUpdate_green();
-  blinkUpdate_red();
+void blink_Update_Red(){
+  static int blinkCount_r = 5;
+  blinkCount_r++;
+  if(blinkCount_r >= blinkLimit){
+    blinkCount_r = 0;
+    red_Control(1);
+  } else {
+    red_Control(0);
+  }
 }
-*/
-void red_control(int on){
+
+void both_Blink(){ //controls and calls the 2 updates for green and red led
+  blink_Update_Green(); //self explanatory
+  blink_Update_Red(); // self explanatory
+}
+
+void red_Control(int on){ //control red LED
   if(on){
     P1OUT |= LED_RED;
   } else{
     P1OUT &= ~LED_RED;
   }
 }
-void green_control(int on){
+void green_Control(int on){ //controls green LED
   if(on){
     P1OUT |= LED_GREEN;
   } else{
     P1OUT &= ~LED_GREEN;
   }
 }
-/*
-void second_Update(){
+
+void second_Update(){ //called 250 times per second 250 calls/ 1 sec = 1 call = 1/250sec
   static int countsec = 0;
   countsec++;
-  if(countsec >= 250){
+  if(countsec >= 250){ 
     countsec = 0;
-    blinkLim();
+    blink_Lim();
   }
 }
 
-void blinkLim(){
-  blinklimit++;
-  if(blinklimit >= 8)
-    blinklimit = 0;
-}*/
+void blink_Lim(){
+  blinkLimit++;
+  if(blinkLimit >= 8)
+    blinkLimit = 0;
+}
+
+void time_Adv_SM(){
+  both_Blink(); // handles green and red LED statemachines
+  second_Update(); // updates seconds called 
+}
+
 void
-__interrupt_vec(WDT_VECTOR) WDT(){
-  blinkCount_r++;
-  blinkCount_g--;
-  if(blinkCount_g <= blinkLimit){
-    blinkCount_g = 6;
-    red_control(1);
-  } else{
-    red_control(0);
-    }
-  if(blinkCount_r >= blinkLimit){
-    blinkCount_r = 0;
-    green_control(1);
-  } else {
-    green_control(0);
-  }  
-  countsec++;
-  if (countsec >= 250) { /* once each sec... */
-    countsec = 0; /* reset count */
-    blinkLimit++;   
-    if(blinkLimit >= 8){
-      blinkLimit = 0;
-    }
-  }
+__interrupt_vec(WDT_VECTOR) WDT(){ //250interrupts/1sec
+  //advances state machine
+  time_Adv_SM();
 }
