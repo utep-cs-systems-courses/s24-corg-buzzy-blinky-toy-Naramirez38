4ef2c57e398d9b409b51c93de31eb914f22562c6
Naramirez38 <naramirez38@miners.utep.edu>
2024-04-25 18:39:48 -0600

    commiting before change to full modularization of methods to make state machine

--- project/2_test/main.c
+++ project/2_test/main.c
@@ -15,18 +15,86 @@ int main(void){
   or_sr(0x18);
 
 }
-
 int countsec = 0;
-int blinklimit = 5;
-int blinkCount = 0; 
+int blinkLimit = 0;
+int blinkCount_r = 5;
+int blinkCount_g = 6;
 
+/*void blinkUpdate_green(){
+  blinkCount_g--;
+  if(blinkCount_g <= blinkLimit){
+    blinkCount_g=6;
+    red_control(1);
+  } else {
+    red_control(0);
+  }
+}
+void blinkUpdate_red(){
+  blinkCount_r++;
+  if(blinkCount_r >= blinkLimit){
+    blinkCount_r = 0;
+    green_control(1);
+  } else {
+    green_control(0);
+  }
+}
+
+void bothBlink(){
+  blinkUpdate_green();
+  blinkUpdate_red();
+}
+*/
+void red_control(int on){
+  if(on){
+    P1OUT |= LED_RED;
+  } else{
+    P1OUT &= ~LED_RED;
+  }
+}
+void green_control(int on){
+  if(on){
+    P1OUT |= LED_GREEN;
+  } else{
+    P1OUT &= ~LED_GREEN;
+  }
+}
+/*
+void second_Update(){
+  static int countsec = 0;
+  countsec++;
+  if(countsec >= 250){
+    countsec = 0;
+    blinkLim();
+  }
+}
+
+void blinkLim(){
+  blinklimit++;
+  if(blinklimit >= 8)
+    blinklimit = 0;
+}*/
 void
 __interrupt_vec(WDT_VECTOR) WDT(){
+  blinkCount_r++;
+  blinkCount_g--;
+  if(blinkCount_g <= blinkLimit){
+    blinkCount_g = 6;
+    red_control(1);
+  } else{
+    red_control(0);
+    }
+  if(blinkCount_r >= blinkLimit){
+    blinkCount_r = 0;
+    green_control(1);
+  } else {
+    green_control(0);
+  }  
   countsec++;
   if (countsec >= 250) { /* once each sec... */
     countsec = 0; /* reset count */
-    P1OUT ^= LED_GREEN; /* toggle red LED */
-    P1OUT ^= LED_RED; /* toggle green LED*/
+    blinkLimit++;   
+    if(blinkLimit >= 8){
+      blinkLimit = 0;
+    }
   }
-
 }
